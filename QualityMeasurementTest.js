75af50688978bf4231b54e3eff0db5c919247ff1
VishalVasam12 <VishalVasam12>
2019-12-30 15:31:16 +0530

    ADD:Testcase for 1 inch is equal to 1 inch

--- QualityMeasurementTest.js
+++ QualityMeasurementTest.js
@@ -1,6 +1,6 @@
 let assert=require("assert");
 let chai=require("chai");
-let QualityMeasurement=require("../QualityMeasurement.js")
+let QualityMeasurement=require("./QualityMeasurement.js")
 describe("test cases for 1 inch is equal 12 feets",function(){
     it("given 1 inches should return 1 inches",function(){
         var resultInInches=QualityMeasurement.convertToInches(1);
